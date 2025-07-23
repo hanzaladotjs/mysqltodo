dcc82a9a59deb03f2831880b7b88f2fbd958788c
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-23 16:15:38 +0530

    aww

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -7,7 +7,7 @@ async function UserSchema () {
 
 
     try{
-        await mydb.query(`DROP TABLE IF EXISTS users`)
+        // await mydb.query(`DROP TABLE IF EXISTS users`)
 await mydb.query(`
     CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
