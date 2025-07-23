55919ba512e1c7db437dcd22d889aa1aa50150e0
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-23 12:37:15 +0530

    server owkay

--- server/src/models/todo.model.ts
+++ server/src/models/todo.model.ts
@@ -2,23 +2,24 @@ import mydb from "../utils/db";
 
 
 async function todoSchema() {
-    try{
-    await mydb.query(`
+    try {
+        //  await mydb.query(`DROP TABLE IF EXISTS todos;`)
+         
+        await mydb.query(`
         CREATE TABLE IF NOT EXISTS todos  (
         id INT AUTO_INCREMENT PRIMARY KEY,
         userid INT,
         name VARCHAR(50) NOT NULL,
         detail VARCHAR(500),
         checkbox BOOLEAN,
-        FOREIGN KEY (userid) REFERENCES userS(id)
-
+        FOREIGN KEY (userid) REFERENCES users(id)
         )
-        
-        
 `)
-    }catch(e) {
+    } catch (e) {
         console.log("error")
     }
 }
+
+todoSchema()
 
 export default todoSchema
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,16 +1,20 @@
 import mydb from "../utils/db";
 
 
+
 async function UserSchema () {
 
+
+
     try{
+        await mydb.query(`DROP TABLE IF EXISTS users`)
 await mydb.query(`
     CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(255) NOT NULL
+    
 )
     `)
 }catch(e){
@@ -18,5 +22,7 @@ await mydb.query(`
 }
 
 }
+
+UserSchema()
 
 export default UserSchema
